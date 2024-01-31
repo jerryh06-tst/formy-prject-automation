0e1212275650dd064faf7b9698b086a37d838544
Jerry <jerry.hernandez15@gmail.com>
2024-01-31 12:09:54 -0700

    git certification

--- test/Form.java
+++ test/Form.java
@@ -1,13 +1,30 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.rmi.Remote;
+
 import static org.junit.Assert.assertEquals;
 
 public class Form {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
+
+        /*WebDriver driver;
+        String nodeURL = " http://192.168.0.248:4444";
+        DesiredCapabilities cap = DesiredCapabilities.chrome();
+        cap.setBrowserName("chrome");
+        cap.setPlatform(Platform.WINDOWS); */
+
+        //ChromeOptions options = new ChromeOptions();
+        //driver = new RemoteWebDriver(new URL(nodeURL),cap);
+        //TEST GIT
 
         System.setProperty("webdriver.chrome.driver", "/Users/beren/Downloads/chromedriver-win64/chromedriver-win64/chromedriver.exe");
 
